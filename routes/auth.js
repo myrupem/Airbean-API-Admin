bac33fb5a43c02e470fcea19b0238c98e261c376
Emilia Myrup <myrupemilia@gmail.com>
2025-06-10 11:16:30 +0200

    crypterat password auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import User from "../models/user.js";
 import { generatePrefixedId } from "../utils/IdGenerator.js";
 import { createUser, findUserByUsername } from "../services/user.js";
+import bcrypt from 'bcrypt'
 
 import { validateAuthBody } from "../middlewares/validators.js";
 
@@ -18,11 +19,13 @@ router.post("/register", validateAuthBody, async (req, res) => {
       return res.status(400).json({ message: "Användarnamnet är redan taget" });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     const userId = generatePrefixedId("user");
     const newUser = await createUser({
       userId,
       username,
-      password,
+      password : hashedPassword,
       role: "user",
     });
 
@@ -58,13 +61,9 @@ router.post("/login", validateAuthBody, async (req, res) => {
 
   try {
     const user = await User.findOne({ username });
-    if (!user || user.password !== password) {
-      return res
-        .status(401)
-        .json({ message: "Felaktigt användarnamn eller lösenord" });
-    }
-
-    global.user = {
+    const isSame = await bcrypt.compare(password, user.password)
+    if (user && isSame) {
+      global.user = {
       userId: user.userId,
       username: user.username,
       role: user.role,
@@ -74,6 +73,11 @@ router.post("/login", validateAuthBody, async (req, res) => {
       message: "Inloggning lyckades",
       user: global.user,
     });
+    } else {
+      return res
+        .status(401)
+        .json({ message: "Felaktigt användarnamn eller lösenord" });
+    }
   } catch (err) {
     res.status(500).json({ message: "Fel vid inloggning", error: err.message });
   }
